82d80c02f892f9669ea4966d96ba541272a6872b
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-11-02 19:13:47 -0400

    Basic movement set up

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehavior : MonoBehaviour {
+    public float xSpeed = 10; //speed moving left and right
+    public float ySpeed = 10;// speed moving up and down
+    public bool facingRight;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    public void Move(float moveX, float moveY)
+    {
+        rb.velocity = new Vector2 (moveX * xSpeed, moveY * ySpeed);
+
+
+        // If the input is moving the player right and the player is facing left...
+        if (moveX > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (moveX < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterBehavior))]
+public class PlayerController : MonoBehaviour {
+
+    private CharacterBehavior character; //character controller attached to player object
+    [HideInInspector]
+
+    // Use this for initialization
+    void Start () {
+        character = GetComponent<CharacterBehavior>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        character.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        
+	}
+}
